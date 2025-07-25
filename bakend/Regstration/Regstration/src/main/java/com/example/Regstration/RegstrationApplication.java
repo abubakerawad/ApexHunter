44fc5469efcd64151ebc6412814bcd430b5ba8f5
abubaker <abubakerawad1980@gmail.com>
2025-07-25 12:58:09 +0200

    init project

---
+++ bakend/Regstration/Regstration/src/main/java/com/example/Regstration/RegstrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Regstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegstrationApplication.class, args);
+	}
+
+}
