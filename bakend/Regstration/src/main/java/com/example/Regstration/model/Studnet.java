d18a7d3123ba8eabaed36920c45fbea29f5da1d8
abubakerawad <abubakerawad1980@gmail.com>
2026-03-18 04:01:39 +0200

    creat model(Studant,Enum)

---
+++ bakend/Regstration/src/main/java/com/example/Regstration/model/Studnet.java
@@ -0,0 +1,17 @@
+package com.example.Regstration.model;
+
+import com.example.Regstration.model.enums.Gender;
+
+import java.util.Date;
+
+public class Studnet {
+    private int id;
+    private  String name;
+    private String phone;
+    private String age;
+    private String address;
+    private Date datecreat;
+    private Date dateupdet;
+    private Gender gender;
+
+}
---
+++ bakend/Regstration/src/main/java/com/example/Regstration/model/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.example.Regstration.model.enums;
+
+public enum Gender {
+    MALE, FEMALE
+}
